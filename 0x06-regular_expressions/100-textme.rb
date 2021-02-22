4e2af2beeace1b70ecb52ee369f807df3177ae6e
Matias Acosta <2277@holbertonschool.com>
2021-02-22 18:15:41 -0300

    Advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?[[:alnum:]]*|[[:alnum:]]*)\] 
-\[to:(\+?[[:alnum:]]*|[[:alnum:]]*)\] \[flags:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?[[:alnum:]]*|[[:alnum:]]*)\] \[to:(\+?[[:alnum:]]*|[[:alnum:]]*)\] \[flags:(.*?)\]/).join(",")
