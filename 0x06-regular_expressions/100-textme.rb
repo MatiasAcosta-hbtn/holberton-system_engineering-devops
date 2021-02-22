1159fc26343ad8d141540d50dd02d2353360e3d6
Matias Acosta <2277@holbertonschool.com>
2021-02-22 18:13:06 -0300

    Advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[a-zA-z]*:\+[0-9]+/).join
+puts ARGV[0].scan(/\[from:(\+?[[:alnum:]]*|[[:alnum:]]*)\] 
+\[to:(\+?[[:alnum:]]*|[[:alnum:]]*)\] \[flags:(.*?)\]/).join(",")
