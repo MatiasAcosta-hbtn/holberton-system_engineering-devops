c82fed2a40e7785530463967372fe131d7b460df
Matias Acosta <2277@holbertonschool.com>
2021-04-06 14:41:51 -0300

    Advanced Task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,31 @@
-#Configure nginx server
+#  Install and configure Nginx web server
 
-package {'nginx':
-  ensure => present,
+exec { 'apt-get_update':
+  command => 'apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'present',
   name   => 'nginx',
 }
 
-file {'/var/www/html/index.html':
-  ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School\n',
+file { 'index.html':
+  ensure  => 'present',
+  path    => '/etc/nginx/html/index.html',
+  content => 'Holberton School',
 }
 
-file_line { 'redirect_me':
+file_line { 'default':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  after  => 'listen [::]:80 default_server;',
+  line   => 'rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
-service { 'nginx':
+service {'nginx':
   ensure     => running,
   enable     => true,
   hasrestart => true,
   require    => Package['nginx'],
-  subscribe  => File_line['redirect_me'],
+  subscribe  => File_line['default'],
 }
