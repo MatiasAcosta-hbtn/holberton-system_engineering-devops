88b80b01ab6f1415725f7103ee2c1c6969664ede
Matias Acosta <2277@holbertonschool.com>
2021-04-06 13:39:14 -0300

    Advanced task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,13 +11,13 @@ package { 'nginx':
 
 file { 'index.html':
   ensure  => 'present',
-  path    => '/etc/nginx/html',
+  path    => '/var/www/html/index.html',
   content => 'Holberton School',
 }
 
 file { '404.html':
   ensure  => 'present',
-  path    => '/etc/nginx/html',
+  path    => '/var/www/html/404.html',
   content => 'Ceci n\'est pas une page',
 }
 
@@ -25,12 +25,12 @@ file {'default':
     ensure  => 'present',
     content => 'listen 80;
     listen [::]:80 default_server;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
-    redirect ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    root   /var/www/html;
+    index  index.html index.htm index.nginx-debian.html;
+    rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;    
     error_page 404 /404.html;
     location /404 {
-      root /etc/nginx/html;
+      root /var/www/html;
       internal;
     }',
     path    => '/etc/nginx/sites-available/default',
