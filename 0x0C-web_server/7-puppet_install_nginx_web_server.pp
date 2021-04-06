08d41be315bd9ed3f3e939a1e60c40c3b0bd754c
Matias Acosta <2277@holbertonschool.com>
2021-04-06 13:33:50 -0300

    Task 6

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,42 @@
+#Configure nginx server
+
+exec { 'apt-get_update':
+  command => 'apt-get update',
+}
+
+package { 'nginx':
+  ensure   => 'installed',
+  provider => 'apt-get',
+}
+
+file { 'index.html':
+  ensure  => 'present',
+  path    => '/etc/nginx/html',
+  content => 'Holberton School',
+}
+
+file { '404.html':
+  ensure  => 'present',
+  path    => '/etc/nginx/html',
+  content => 'Ceci n\'est pas une page',
+}
+
+file {'default':
+    ensure  => 'present',
+    content => 'listen 80;
+    listen [::]:80 default_server;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+    redirect ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }',
+    path    => '/etc/nginx/sites-available/default',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
