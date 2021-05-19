68b4be38bdb707de5c712b10317a716e0acfed69
Matias Acosta <2277@holbertonschool.com>
2021-05-18 21:26:49 -0300

    Fix Refactor

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Working with Reddit API"""
+"""Reddit API"""
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """Function that recursively returns a list of titles
-    for all hot articles on a given subreddit"""
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "Refactor/2.0.21"
