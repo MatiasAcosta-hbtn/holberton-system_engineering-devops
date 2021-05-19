877398f2563d435ac816ab21427f3d10cf82618b
Matias Acosta <2277@holbertonschool.com>
2021-05-18 21:24:53 -0300

    Fix Refactor

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ def recurse(subreddit, hot_list=[]):
     for all hot articles on a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Refactor/2.0.21"
     }
     params = {
         "after": after,
